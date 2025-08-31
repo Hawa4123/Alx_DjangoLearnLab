88a67632be3f6ee144cdb92da0a67f817ec781cc
Nambuya <hawanambuya@gmail.com>
2025-08-31 23:21:04 +0300

    Initial Django project setup: LibraryProject

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# 1️⃣ Book model
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
@@ -9,18 +8,16 @@ class Book(models.Model):
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
 
-# 2️⃣ Author model
+
 class Author(models.Model):
     name = models.CharField(max_length=100)
-    birth_year = models.IntegerField()
 
     def __str__(self):
-        return f"{self.name} ({self.birth_year})"
+        return self.name
 
-# 3️⃣ Publisher model
+
 class Publisher(models.Model):
-    name = models.CharField(max_length=150)
-    city = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.name}, {self.city}"
+        return self.name
