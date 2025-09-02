2ae85fbd2e6180bd6579887991d69a9a66cbf4c1
Nambuya Hawa <hawanambuya@gmail.com>
2025-09-02 23:06:10 +0300

    Add files via upload

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,42 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+# -------------------------
+# Query 1: All books by a specific author
+# -------------------------
+def get_books_by_author(author_name):
+    try:
+        author = Author.objects.get(name=author_name)
+        return author.books.all()
+    except Author.DoesNotExist:
+        return []
+
+
+# -------------------------
+# Query 2: All books in a library
+# -------------------------
+def get_books_in_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.books.all()
+    except Library.DoesNotExist:
+        return []
+
+
+# -------------------------
+# Query 3: Retrieve the librarian for a library
+# -------------------------
+def get_librarian_for_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.librarian
+    except (Library.DoesNotExist, Librarian.DoesNotExist):
+        return None
+
+
+# -------------------------
+# Sample usage
+# -------------------------
+if __name__ == "__main__":
+    print("Books by J.K. Rowling:", list(get_books_by_author("J.K. Rowling")))
+    print("Books in Central Library:", list(get_books_in_library("Central Library")))
+    print("Librarian of Central Library:", get_librarian_for_library("Central Library"))
