7cca1abdd867c0b5e7c279c1803ae33258af8b8d
Nambuya Hawa <hawanambuya@gmail.com>
2025-09-05 10:55:34 +0300

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.shortcuts import render
-from django.contrib.auth.decorators import user_passes_test
 
 class UserProfile(models.Model):
     ROLE_CHOICES = (
@@ -17,14 +15,7 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
-  # Helper function to check if user is Admin
-def is_admin(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
-# Admin-only view
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'admin_view.html')  # Template for Admin users
- 
+
 # Signal to automatically create UserProfile when a new User is created
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
