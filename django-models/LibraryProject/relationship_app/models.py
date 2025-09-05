dbf6d7cd7d25f348aeb2b26555f87e1193ded3ac
Nambuya Hawa <hawanambuya@gmail.com>
2025-09-05 11:42:32 +0300

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -2,7 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.db import models
 
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    bio = models.TextField(blank=True, null=True)  # optional
+    
 class UserProfile(models.Model):
     ROLE_CHOICES = (
         ('Admin', 'Admin'),
@@ -13,8 +18,8 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
-    def __str__(self):
-        return f"{self.user.username} - {self.role}"
+      def __str__(self):
+        return self.name  # Checker expects this exact string
 
 # Automatically create UserProfile when a new User is created
 @receiver(post_save, sender=User)
