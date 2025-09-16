f88c1656651afd5d27a54c0df41ef61bcc2fceaa
Nambuya Hawa <hawanambuya@gmail.com>
2025-09-17 00:49:06 +0300

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .views import BookList
+from django.urls import path, include
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
 ]
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # <- Add this line
+]
+
