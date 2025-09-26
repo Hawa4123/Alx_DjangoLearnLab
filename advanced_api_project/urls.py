b193276ce047298aee3cc05a5fbcfbae156f761f
Nambuya Hawa <hawanambuya@gmail.com>
2025-09-26 16:08:05 +0300

    Update urls.py

--- advanced_api_project/urls.py
+++ advanced_api_project/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+def home(request):
+    return HttpResponse("Hello, Django is working!")
+
+urlpatterns = [
+    path('', home),
 ]
