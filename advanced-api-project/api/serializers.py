4cbb4d01a6270e867bc669c8b2200c3cf89ca861
Nambuya Hawa <hawanambuya@gmail.com>
2025-09-27 23:11:42 +0300

    Update serializers.py

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -5,3 +5,11 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+
+    def validate_title(self, value):
+        """
+        Example validation: ensure the book title is not empty.
+        """
+        if not value:
+            raise serializers.ValidationError("Title cannot be empty.")
+        return value
