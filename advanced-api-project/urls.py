28f562560269de45a52685d797df3b4885018cb4
Nambuya Hawa <hawanambuya@gmail.com>
2025-09-28 08:30:55 +0300

    Update urls.py

--- advanced-api-project/urls.py
+++ advanced-api-project/urls.py
@@ -1,8 +1,10 @@
-<<<<<<< HEAD
+
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from .views import YourModelViewSet
+from django.contrib import admin
+
 
 router = DefaultRouter()
 router.register(r'yourmodel', YourModelViewSet, basename='yourmodel')
@@ -11,16 +13,19 @@ urlpatterns = [
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('', include(router.urls)),
 ]
-=======
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # <- include the API app URLs
+]
 """
 URL configuration for advanced_api_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-<<<<<<< HEAD
+
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
-=======
+
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
->>>>>>> efee385d6c8f7a33a3a8e8e44a97537e13aecb57
+
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -41,4 +46,4 @@ urlpatterns = [
     ]
 
 
->>>>>>> e2b71aa3a299cf5cc532066e2d6f76fbfac0e3e6
+
