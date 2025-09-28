3e5fc3ffff4dd112a7aa97db8b8b3d5c4861dc4a
Nambuya Hawa <hawanambuya@gmail.com>
2025-09-28 08:35:55 +0300

    Update urls.py

--- advanced-api-project/urls.py
+++ advanced-api-project/urls.py
@@ -1,51 +1,24 @@
-
+# advanced-api-project/urls.py
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
-from .views import YourModelViewSet
-from django.contrib import admin
-from django.urls import path, include  # include is required
+from api.views import YourModelViewSet  # adjust this import if needed
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # <- include the API app URLs
-]
-
+# Create DRF router for viewsets
 router = DefaultRouter()
 router.register(r'yourmodel', YourModelViewSet, basename='yourmodel')
 
 urlpatterns = [
+    # Admin site
+    path('admin/', admin.site.urls),
+
+    # Include all API app URLs
+    path('api/', include('api.urls')),
+
+    # Token authentication endpoint
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+
+    # Include DRF router URLs
     path('', include(router.urls)),
 ]
-
-"""
-URL configuration for advanced_api_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    ]
-
-
-
