d4aeabd510f01f0388c4d05ea1144339f2ac9c21
Nambuya Hawa <hawanambuya@gmail.com>
2025-09-29 06:51:12 +0300

    Update serializers.py

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -5,3 +5,9 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['id', 'title', 'publication_year', 'author']
+
+    # Custom validation for publication_year
+    def validate_publication_year(self, value):
+        if value and (value < 0 or value > 2100):
+            raise serializers.ValidationError("Publication year must be between 0 and 2100.")
+        return value
