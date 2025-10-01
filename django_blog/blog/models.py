b70d62fe001dc59df296821d27573eb3e73068e4
Nambuya Hawa <hawanambuya@gmail.com>
2025-09-30 20:25:01 -0700

    Add authentication: register/login/logout/profile + Profile model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(blank=True)
+    image = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
 
     def __str__(self):
-        return self.title
-<<<<<<< HEAD
-=======
+        return f"{self.user.username} Profile"
 
->>>>>>> 932d7d079c50c8402369b362e7796077cc771de9
+# Create or update Profile when User is saved
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -1,26 +1,15 @@
-"""
-URL configuration for django_blog project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-<<<<<<< HEAD
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-=======
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
->>>>>>> 932d7d079c50c8402369b362e7796077cc771de9
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),  # auth-related URLs (register, login, profile)
+    # You can add blog home or posts URLs here later. For now a simple index:
+    path('', TemplateView.as_view(template_name='blog/index.html'), name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
