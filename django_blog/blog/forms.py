1913f1c0873fcd2348d98e92eb5d9741cd0bce55
Nambuya Hawa <hawanambuya@gmail.com>
2025-10-01 07:25:51 +0300

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Post
 from django.contrib.auth.models import User
+from .models import Comment
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -10,3 +11,15 @@ class PostForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'placeholder': 'Post title', 'class': 'form-control'}),
             'content': forms.Textarea(attrs={'placeholder': 'Write your post here...', 'class': 'form-control', 'rows': 10}),
         }
+        class CommentForm(forms.ModelForm):
+    content = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'rows': 3, 'placeholder': 'Add a comment...'}
+        ),
+        label='',
+        max_length=1000  # optional: limit comment length
+    )
+
+    class Meta:
+        model = Comment  # links form to Comment model
+        fields = ['content']  # only allow editing of content
