72d1e8565a0b434fd471e5fac5556ebd1d30340b
Nambuya Hawa <hawanambuya@gmail.com>
2025-10-12 22:53:46 +0300

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -109,6 +109,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  # PostgreSQL backend
+        'NAME': 'social_media_db',                  # Database name
+        'USER': 'db_user',                           # Your DB user
+        'PASSWORD': 'db_password',                   # Your DB password
+        'HOST': 'localhost',                          # Usually 'localhost' or your DB host
+        'PORT': '5432',                               # PostgreSQL default port
     }
 }
 
