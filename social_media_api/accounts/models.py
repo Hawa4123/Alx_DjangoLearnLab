8a1864cb0afb7ae5ec96578180dd028050744efd
Nambuya Hawa <hawanambuya@gmail.com>
2025-10-12 23:44:58 +0300

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -11,6 +11,13 @@ class CustomUser(AbstractUser):
         symmetrical=False,
         related_name='followers',
         blank=True
+        bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    following = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='followers',
+        blank=True
     )
 
     def __str__(self):
