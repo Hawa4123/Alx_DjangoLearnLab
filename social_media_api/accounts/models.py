1dcfde7c108cf5e0b268c59a64cd4663ac752532
Nambuya Hawa <hawanambuya@gmail.com>
2025-10-12 23:11:20 +0300

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    """
+    Custom user model that extends Django's AbstractUser
+    and includes a 'following' field for following other users.
+    """
+    following = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='followers',
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
